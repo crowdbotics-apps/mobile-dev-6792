cc010b5003cfaa6b40a6ad68f3a19dad20f01bcb
Crowdbotics <team@crowdbotics.com>
2020-06-29 13:24:06 +0000

    add package blueprint-react-native-message-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Messaging7131', human_name: 'Messaging', access_route: 'Messaging7131', icon: 'comments'},
 { name: 'EmailAuth7130', human_name: 'Email Auth', access_route: 'EmailAuth7130', icon: 'envelope-o'},
 { name: 'CalendarView7129', human_name: 'Calendar View', access_route: 'CalendarView7129', icon: 'calendar'},
 { name: 'BlankScreen47128', human_name: 'Blank Screen 4', access_route: 'BlankScreen47128'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Messaging7131Navigator from '../features/Messaging7131/navigator';
 import EmailAuth7130Navigator from '../features/EmailAuth7130/navigator';
 import CalendarView7129Navigator from '../features/CalendarView7129/navigator';
 import BlankScreen47128Navigator from '../features/BlankScreen47128/navigator';
@@ -21,6 +22,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+Messaging7131: { screen: Messaging7131Navigator },
 EmailAuth7130: { screen: EmailAuth7130Navigator },
 CalendarView7129: { screen: CalendarView7129Navigator },
 BlankScreen47128: { screen: BlankScreen47128Navigator },
